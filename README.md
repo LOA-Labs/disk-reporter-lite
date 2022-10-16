794c967f49767b4d180570889f41a395cb01423e
papl.co <ceu@papl.co>
2022-10-16 20:48:07 -0300

    log req ip

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# LOA Labs Node Monitor
+# LOA Labs Disk Reporter Lite
 
-Nodejs toolset to monitor validator uptime and diskspace, automate rewards and restaking, monitor and vote on governance proposals. 
-
-Takes a lightweight approach to node monitoring and automation. Nodejs instance can run on its own 
-
-Single instance can monitor multiple chains and connect to multiple notification channels such as Slack, Discord, Telegram, and Twitter.
-
-Leverages authz to delegate a sub-set of needed authorizations to perform automated tasks. 
-
-Each service runs on its own cron schedule, frequency of checkins and notifications can be customized.
+Lightweight nodejs disk status endpoint.
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,24 @@ const express = require('express');
 import { Request, Response } from 'express';
 import checkDiskSpace from 'check-disk-space'
 /*
-iptables -I INPUT -p tcp -s 0.0.0.0 --dport 4445 -j ACCEPT
 iptables -I INPUT -p tcp -s 0.0.0.0/0 --dport 4445 -j DROP
+iptables -I INPUT -p tcp -s 0.0.0.0 --dport 4445 -j ACCEPT
+iptables -I INPUT -p tcp -s 167.99.56.130 --dport 4445 -j ACCEPT
+screen -R drl
+git clone https://github.com/LOA-Labs/disk-reporter-lite.git
+cd disk-reporter-lite/
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+..b
+nvm install v17.8.0
+npm i
+npm run start
 */
+
 const startServer = () => {
 	const app = express()
 	const port = 4445
 	app.get('/disk', async (req: Request, res: Response) => {
+		console.log('req from', req.remote_ip)
 		try {
 			let diskSpace: any = await checkDiskSpace(req.query.disk)
 			diskSpace.percent = Math.ceil((diskSpace.size - diskSpace.free) / diskSpace.size * 100).toString()
